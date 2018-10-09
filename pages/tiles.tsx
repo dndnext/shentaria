726cdc1640c93a5005e8a65f5675f73dcd0c7f70
Matthew Elphick <matthew.elphick@threadsstyling.com>
2018-10-09 08:32:53 +0100

    feat: 🎸 Work on image tiling and downloading

--- pages/tiles.tsx
+++ pages/tiles.tsx
@@ -1,3 +1,5 @@
+import { saveAs } from "file-saver";
+import JSZip from "jszip";
 import React, { ChangeEvent } from "react";
 
 interface ImageDetails {
@@ -17,31 +19,63 @@ interface Tile {
 export default class extends React.Component {
   public state = {
     render: false,
-    tileDimension: 100,
+    tileDimension: 256,
   };
 
   private reader?: FileReader;
   private ctx?: CanvasRenderingContext2D;
+  private tileCtx?: CanvasRenderingContext2D;
   private img?: HTMLImageElement;
+  private tiles: Tile[] = [];
 
   public componentDidMount() {
     this.setState({ render: true });
+    const { tileDimension } = this.state;
     this.reader = new FileReader();
     const canvas = document.getElementById("tiles") as HTMLCanvasElement;
     canvas.width = 500;
     canvas.height = 500;
     this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const tileCanvas = document.getElementById("tile") as HTMLCanvasElement;
+    tileCanvas.width = tileDimension;
+    tileCanvas.height = tileDimension;
+    this.tileCtx = tileCanvas.getContext("2d") as CanvasRenderingContext2D;
     this.img = new Image();
   }
 
   public render() {
+    const { tileDimension } = this.state;
     return (
       <div>
         <input type="file" onChange={this.handleFile} />
+        <button onClick={this.download}>Download</button>
+        <input
+          disabled
+          type="range"
+          min={10}
+          max={512}
+          value={tileDimension}
+          onChange={this.handleState("tileDimension")}
+        />
+        <input
+          type="number"
+          min={10}
+          max={512}
+          value={tileDimension}
+          onChange={this.handleState("tileDimension")}
+        />
         <canvas id="tiles" />
+        <canvas id="tile" style={{ display: "none" }} />
       </div>
     );
   }
+
+  private handleState = (field: string) => {
+    return (e: ChangeEvent) => {
+      return; // disabled for now due to call stack issues
+      this.setState({ [field]: (e.target as any).value });
+    };
+  };
 
   private handleFile = (e: ChangeEvent) => {
     const { tileDimension } = this.state;
@@ -62,6 +96,8 @@ export default class extends React.Component {
           tilesY: Math.floor(img.height / tileDimension),
           w: img.width,
         };
+        ctx.canvas.height = details.h;
+        ctx.canvas.width = details.w;
         ctx.drawImage(img, 0, 0);
         details.data = ctx.getImageData(0, 0, details.w, details.h).data;
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -133,9 +169,27 @@ export default class extends React.Component {
   };
 
   private drawTiles = (tiles: Tile[]) => {
-    const offset = 1.1;
+    const offset = 1.01;
     console.log(tiles); //tslint:disable-line
     const ctx = this.ctx!;
     tiles.forEach(d => ctx.putImageData(d.data, d.x * offset, d.y * offset));
+    this.tiles = tiles;
+  };
+
+  private download = async () => {
+    const tileCtx = this.tileCtx!;
+    const images = await Promise.all(
+      this.tiles.map(
+        d =>
+          new Promise(resolve => {
+            tileCtx.putImageData(d.data, 0, 0);
+            tileCtx.canvas.toBlob(blob => resolve(blob as any));
+          }),
+      ),
+    );
+    const zip = new JSZip();
+    images.forEach((img, i) => zip.file(`tile-${i}.png`, img as any));
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "tiles.zip");
   };
 }
