f0ced587dc537787bcc0a03512ee0c39574590d6
Matthew Elphick <matthew.elphick@threadsstyling.com>
2018-10-14 10:25:45 +0100

    feat: 🎸 Add example markers with controllable visible zooms

--- app/components/organisms/MapViewer.tsx
+++ app/components/organisms/MapViewer.tsx
@@ -9,22 +9,86 @@ declare module "react" {
   }
 }
 
-export default class extends React.Component {
+const tempmarkers = [
+  {
+    icon: "default",
+    position: [37, 17.09],
+    text: "Orsham. Totally not Horsham.",
+    visible: [2, 3, 4],
+  },
+  {
+    icon: "default",
+    position: [45, 100.09],
+    text: "Musta. Is it far?",
+    visible: [2, 3, 4],
+  },
+  { icon: "default", position: [5, 50.09], text: "Pearkl", visible: [3, 4] },
+];
+
+interface MarkerInfo {
+  icon: string;
+  position: number[];
+  text: string;
+  visible: number[];
+}
+
+interface State {
+  markers?: MarkerInfo[];
+  render: boolean;
+  z: number;
+}
+
+interface ViewportChange {
+  center: number[];
+  zoom: number;
+}
+
+export default class extends React.Component<{}, State> {
   public state = {
+    markers: [],
     render: false,
+    z: 1,
   };
+
+  private L?: any;
+  private rL?: any;
+  private icons?: any = {};
 
   public componentDidMount() {
     this.setState({ render: true });
+    this.L = require("leaflet");
+    this.rL = require("react-leaflet");
+    this.icons.default = this.L.icon({
+      iconAnchor: [0, 0],
+      iconSize: [10, 10],
+      iconUrl: "/static/markers/default.png",
+      popupAnchor: [5, 2],
+    });
+  }
+
+  public componentDidUpdate(_: {}, { z: oldZ }: State) {
+    const { z } = this.state;
+    if (z !== oldZ) {
+      this.setState({
+        markers: tempmarkers.filter(({ visible }) => visible.includes(z)),
+      });
+    }
   }
 
   public render() {
-    const { render } = this.state;
-    let Map, TileLayer;
+    const { render, markers } = this.state;
+    let Map, TileLayer, Marker: any, Popup: any;
     if (render) {
-      const { Map: map, TileLayer: tilelayer } = require("react-leaflet");
+      const {
+        Map: map,
+        TileLayer: tilelayer,
+        Marker: marker,
+        Popup: popup,
+      } = this.rL;
       Map = map;
       TileLayer = tilelayer;
+      Marker = marker;
+      Popup = popup;
     }
 
     return render ? (
@@ -47,14 +111,17 @@ export default class extends React.Component {
           <Map
             center={[51.505, -0.09]}
             zoom={1}
-            style={{ height: "100vh" }}
+            style={{ height: "75vh" }}
             minZoom={1}
             maxZoom={5}
+            onViewportChanged={this.onChange}
           >
-            <TileLayer
-              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              url="/static/tiles/test/{z}/{x}/{y}.png"
-            />
+            <TileLayer url="/static/tiles/test/{z}/{x}/{y}.png" noWrap />
+            {markers.map(({ icon, position, text }: MarkerInfo, i) => (
+              <Marker key={i} position={position} icon={this.icons[icon]}>
+                <Popup>{text}</Popup>
+              </Marker>
+            ))}
           </Map>
         </NoSSR>
       </div>
@@ -62,4 +129,8 @@ export default class extends React.Component {
       <span>Map Placeholder</span>
     );
   }
+
+  private onChange = ({ zoom: z }: ViewportChange) => {
+    this.setState({ z });
+  };
 }
